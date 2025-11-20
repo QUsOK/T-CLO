978a9608b30c5427b504186e7fc2ba6300102c60
CENDAN <marccendan@gmail.com>
2025-11-20 16:27:49 +0100

    Ajout du NSG pour le ssh

--- terraform/iaas/main.tf
+++ terraform/iaas/main.tf
@@ -1,6 +1,28 @@
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
+resource "azurerm_network_security_group" "nsg" {
+  name                = "vm-demo-nsg"
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
 
 resource "azurerm_public_ip" "vm_ip" {
   name                   = "vm-ip"
