27965cf384a917dd2519927ee5a1675095c99139
CENDAN <marccendan@gmail.com>
2025-11-20 16:31:04 +0100

    Changement de nom pour avoir la ressource

--- terraform/iaas/main.tf
+++ terraform/iaas/main.tf
@@ -2,7 +2,7 @@ data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
 resource "azurerm_network_security_group" "nsg" {
-  name                = "vm-demo-nsg"
+  name                = "vm-nsg"
   location            = var.location
   resource_group_name = data.azurerm_resource_group.rg.name
 
