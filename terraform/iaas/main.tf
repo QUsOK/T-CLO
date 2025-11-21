7b67c26c2d781fbf6a61352947d10e87fbb78dc0
CENDAN <marccendan@gmail.com>
2025-11-21 12:19:36 +0100

    Un s en trop

--- terraform/iaas/main.tf
+++ terraform/iaas/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_network_security_group" "nsg" {
 
 resource "azurerm_subnet_network_security_group_association" "subnet_nsg" {
   subnet_id                 = azurerm_subnet.subnet.id
-  network_interface_ids     = azurerm_network_interface.nic.id
+  network_interface_id     = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
